25e935775e416146f4a44d5404c8adfcd8cc360f
Warahi Spenceley <wizzzo@protonmail.com>
2021-02-26 12:31:18 +1300

    initial commit

--- client/reducers/habitReducer.js
+++ client/reducers/habitReducer.js
@@ -1,19 +1,10 @@
-const habitReducer = (state = [{
-  id: 1,
-  user_id: 1,
-  title: 'updatedrunning',
-  description: 'go for a run everyday',
-  habit_icon: 'some icon',
-  total_goal_count: 0,
-  priority: 4,
-  goal_count: 0
-}], action) => {
+const habitReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_HABIT':
       return action.habit
 
     case 'UPDATE_HABIT':
-      return state.map(habit => habit.id === action.habit.it ? action.habit : habit)
+      return state.map(habit => habit.id === action.habit.id ? action.habit : habit)
 
     case 'DELETE_HABIT':
       return state.filter(habit => habit.id !== action.id)
