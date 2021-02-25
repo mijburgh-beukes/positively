ea8589e55c9db6c3bbff1bebaa8bba60fb2de1e6
jordan-tuinman <jojotuijo@gmail.com>
2021-02-25 15:52:06 +1300

    set nav image icons to a tags - need to switch to links later when router is enabled

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <div className="nav">
       <img src="" alt="app-logo"/>
-      <img src="" alt="dashboard"/>
-      <img src="" alt="add-habit"/>
-      <img src="" alt="profile"/>
-      <img src="" alt="edit-habit"/>
+      {/* TODO: need to be links once router is set up */}
+      {/* TODO: needs image srcs */}
+      <a to=""><img src="" alt="dashboard"/></a>
+      <a to=""><img src="" alt="add-habit"/></a>
+      <a to=""><img src="" alt="profile"/></a>
+      <a to=""><img src="" alt="edit-habit"/></a>
     </div>
   )
 }
