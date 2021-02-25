4ceb4827eeebc288227ee9af3d0c929c5282ee93
Justin Cole <justincole@outlook.co.nz>
2021-02-26 11:26:19 +1300

    adds achievement badges

--- client/components/Achievements.jsx
+++ client/components/Achievements.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-function Achievements (props) {
+function Achievements(props) {
   return (
     <div className="container-sm">
       <h4>Achievements</h4>
       <div className="row row-cols-2">
-        <div className="col-sm-3">Badge1</div>
-        <div className="col-sm-3">Badge2</div>
-        <div className="col-sm-3">Badge3</div>
-        <div className="col-sm-3">NextUp</div>
+        <div className="col-sm-3">
+          <Badge />
+        </div>
+        <div className="col-sm-3">
+          <Badge />
+        </div>
+        <div className="col-sm-3">
+          <Badge />
+        </div>
+        <div className="col-sm-3">
+          <Badge />
+        </div>
       </div>
     </div>
   )
 }
 
-function mapStateToProps (state) {
+function Badge(props) {
+  return (
+    <div className="container">
+      <div className="achievement-badge justify-content-centre">Badge</div>
+    </div>
+  )
+}
+
+function mapStateToProps(state) {
   return {
     user: state.user
   }
