9f777fb3c0c9ac0e553ff8a066d403ea16da2148
mijburgh-beukes <mijburgh.beukes@gmail.com>
2021-02-26 10:54:40 +1300

    Add Achievements Component to Dashboard

--- client/components/Achievements.jsx
+++ client/components/Achievements.jsx
@@ -1,18 +1,24 @@
-import React from 'React'
+import React from 'react'
 import { connect } from 'react-redux'
 
 function Achievements (props) {
   return (
-    <div className="container">
+    <div className="container-sm">
       <h4>Achievements</h4>
       <div className="row row-cols-2">
-        <div className="col">Badge1</div>
-        <div className="col">Badge2</div>
-        <div className="col">Badge3</div>
-        <div className="col">NextUp</div>
+        <div className="col-sm-3">Badge1</div>
+        <div className="col-sm-3">Badge2</div>
+        <div className="col-sm-3">Badge3</div>
+        <div className="col-sm-3">NextUp</div>
       </div>
     </div>
   )
 }
 
-export default connect()(Achievements)
+function mapStateToProps (state) {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(Achievements)
