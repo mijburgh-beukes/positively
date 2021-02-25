5a7cad23e2b9c72384d9e5f147be6f8f13d55be4
jordan-tuinman <jojotuijo@gmail.com>
2021-02-25 16:24:28 +1300

    add file for tests - fakeStore

--- client/components/Profile.test.jsx
+++ client/components/Profile.test.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { provider } from 'react-redux'
-import { } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Profile from './Profile'
+import { fakeStore } from '../testHelpers'
 
 describe('<Profile.jsx />', () => {
+  test('', async () => {
 
+  })
 })
