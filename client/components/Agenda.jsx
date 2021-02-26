8f25bb62e0980db35a9911df6fa345c53825a485
mijburgh-beukes <mijburgh.beukes@gmail.com>
2021-02-26 14:39:52 +1300

    Play with styling"

--- client/components/Agenda.jsx
+++ client/components/Agenda.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 
 function Agenda () {
   return (
-    <div className="container-sm agenda shadow px-4 py-2">
+    <div className="container-sm agenda shadow px-4 pb-2 pt-4">
       <div className="row">
-        <h4>Your top habit it</h4>
+        <h2 className="module-2">Agenda</h2>
+        <h4>Your top performing habit</h4>
         <HabitPH />
         <h4>Habits needing some love</h4>
         <HabitPH />
         <HabitPH />
+        <h3>Keep up that momentum!</h3>
       </div>
     </div>
   )
 }
-
+// This is just a placeholder for the habit components
 function HabitPH () {
   return (
     <div className="habitPH mb-2 rounded-3">
