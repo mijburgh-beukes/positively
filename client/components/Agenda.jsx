a48e9d0ba8e6cfd09c8815019b4030b3a61165e7
mijburgh-beukes <mijburgh.beukes@gmail.com>
2021-02-26 15:39:19 +1300

    Finish acheivements initial styling

--- client/components/Agenda.jsx
+++ client/components/Agenda.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 function Agenda () {
   return (
-    <div className="container-sm agenda shadow px-4 pb-2 pt-4">
+    <div className="container-sm agenda shadow px-3 pb-2 pt-3">
       <div className="row">
-        <h2 className="module-2">Agenda</h2>
+        <h2 className="module-h2">Agenda</h2>
         <h4>Your top performing habit</h4>
         <HabitPH />
         <h4>Habits needing some love</h4>
         <HabitPH />
         <HabitPH />
-        <h3>Keep up that momentum!</h3>
+        <h2>Keep up that momentum!</h2>
       </div>
     </div>
   )
