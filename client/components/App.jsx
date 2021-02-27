b1207db7e4fe90fe001f2d19d317f49008f23de7
Warahi Spenceley <wizzzo@protonmail.com>
2021-02-27 20:05:55 +1300

    mode AddHabit to dashboard

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,7 +8,6 @@ import { setUser } from '../actions'
 import Nav from './Nav'
 import Dashboard from './Dashboard'
 import Profile from './Profile'
-import AddHabit from './AddHabit'
 
 import { Route, Switch } from 'react-router-dom'
 
@@ -29,10 +28,7 @@ function App ({ dispatch, user }) {
     <div className="app">
       <Nav />
       <Switch>
-        <Route path="/user/:id">
-          <Profile />
-        </Route>
-        <Route path="/habit" component={AddHabit} />
+        <Route path="/user/:id" component={Profile} />
         <Route exact path="/" component={Dashboard} />
       </Switch>
     </div>
--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -5,12 +5,15 @@ import Achievements from './Achievements'
 import Agenda from './Agenda'
 import UserDetails from './UserDetails'
 
+import AddHabit from './AddHabit'
+
 const Dashboard = () => {
   return (
     <div>
       <Agenda />
       <Achievements />
       <UserDetails />
+      <AddHabit />
     </div>
   )
 }
