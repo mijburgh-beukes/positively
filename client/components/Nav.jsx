1de5d46f20b7b77fc0694f89e6a03239cd020ff5
mijburgh-beukes <mijburgh.beukes@gmail.com>
2021-03-01 23:25:17 +1300

    Start playing with flex settings

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,20 +4,24 @@ import { dashboardSVG, addHabitSVG, profileSVG, edithabitsSVG } from './NavItems
 
 const Nav = () => {
   return (
-    <div className="nav d-flex flex-lg-column flex-lg-row rounded-3 ">
-      <img src="/src/logo.svg" alt="app-logo" className="logo m-3" />
-      <NavLink exact={true} to="/">
-        {dashboardSVG}
-      </NavLink>
-      <NavLink to="/addHabit">
-        {addHabitSVG}
-      </NavLink>
-      <NavLink to="/user/:id">
-        {profileSVG}
-      </NavLink>
-      <NavLink to="/edithabits">
-        {edithabitsSVG}
-      </NavLink>
+    <div className="nav d-flex flex-lg-column flex-sm-row rounded-3 align-items-center px-2 p-md-2">
+      <div className="">
+        <img src="/src/logo.svg" alt="app-logo" className="logo me-sm-2 mb-md-5" />
+      </div>
+      <div className="navItems d-flex flex-lg-column flex-sm-row">
+        <NavLink exact={true} to="/">
+          {dashboardSVG}
+        </NavLink>
+        <NavLink to="/addHabit">
+          {addHabitSVG}
+        </NavLink>
+        <NavLink to="/user/:id">
+          {profileSVG}
+        </NavLink>
+        <NavLink to="/edithabits">
+          {edithabitsSVG}
+        </NavLink>
+      </div>
     </div>
   )
 }
