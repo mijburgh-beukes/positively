f867056e3550ded80bb079d50a7201873d5e8533
mijburgh-beukes <mijburgh.beukes@gmail.com>
2021-03-02 10:46:24 +1300

    Add home link to logo

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -6,7 +6,9 @@ const Nav = () => {
   return (
     <div className="navBar d-flex flex-lg-column flex-sm-row rounded-3 px-2 p-md-2">
       <div className="logoContainer pt-lg-2 img-fluid">
-        <img src="/src/logo.svg" alt="app-logo" className="logo" />
+        <NavLink exact={true} to="/">
+          <img src="/src/logo.svg" alt="app-logo" className="logo" />
+        </NavLink>
       </div>
       <div className="navItems pt-lg-4 d-flex flex-lg-column flex-sm-row">
         <NavLink exact={true} to="/">
