d9c99d0fc8971fb67ef57fdf3b221a3b3048874c
Warahi Spenceley <wizzzo@protonmail.com>
2021-03-01 15:00:15 +1300

    habit tests fix

--- server/routes/habit.js
+++ server/routes/habit.js
@@ -10,29 +10,20 @@ router.post('/', (req, res) => {
   const habit = req.body
   db.addHabit(habit)
     .then((habit) => {
-      console.log(habit[0])
       return db.getHabit(habit[0])
-      // res.body
-      // return null
     })
     .then((newHabit) => {
-      res.json(newHabit)
+      res.status(201).json(newHabit)
       return null
     })
     .catch(err => res.status(500).send('DATABASE ERROR: ' + err.message))
 })
-
-// router.get('/', (req, res) => {
-//   db.getHabits()
-
-// })
 
 router.patch('/:id', (req, res) => {
   const userId = Number(req.params.id)
   const changes = req.body
   db.editHabit(userId, changes)
     .then(habit => res.json(habit))
-    // .then(() => res.sendStatus(200))
     .catch(err => res.status(500).send('DATABASE ERROR: ' + err.message))
 })
 
