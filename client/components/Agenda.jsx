63957a85c7691e59144858328140072b768b33f9
mijburgh-beukes <mijburgh.beukes@gmail.com>
2021-03-03 12:18:42 +1300

    Replace Agenda top habit with welcome message

--- client/components/Agenda.jsx
+++ client/components/Agenda.jsx
@@ -7,30 +7,19 @@ import { orderedHabitsByGoalCount } from '../utils'
 
 function Agenda ({ user }) {
   const orderedHabits = orderedHabitsByGoalCount(user)
-  const lengthOfOrderedHabits = orderedHabits ? orderedHabits.length - 1 : null
 
   // TODO: Possible opportunity for factoriseation
-  const habitWithHighestGC = orderedHabits ? lengthOfOrderedHabits : null
   const habitWithLowestGC = orderedHabits ? orderedHabits[0] : null
   const habitWith2ndLowestGC = orderedHabits ? orderedHabits[1] : null
   return (
     <div className="bg-white shadow-sm rounded-3 px-3 pb-1 pt-2 mb-3 text-midnight">
       <div className="row">
         <h3 className="mb-3">Agenda</h3>
-        <h5>Your top performing habit</h5>
-        {orderedHabits && (
-          <div className="mb-2">
-            <HabitListItem
-              key={habitWithHighestGC.id}
-              habit={habitWithHighestGC}
-              user={user}
-            />
-          </div>
-        )}
+        <p>Welcome back! We&apos;ve curated some items for you to focus on&nbsp;today...</p>
         {habitWithLowestGC && (
           <>
             <h5>Habits needing some love</h5>
-            <div /* className="habitPH mb-2 rounded-3" */>
+            <div>
               <HabitListItem
                 key={habitWithLowestGC.id}
                 habit={habitWithLowestGC}
