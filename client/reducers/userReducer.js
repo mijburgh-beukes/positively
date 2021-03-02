a79630f691df2f242f3bbb37b5549ab5b4e3066e
Warahi Spenceley <wizzzo@protonmail.com>
2021-03-02 15:46:51 +1300

    DELETE, UPDATE, ADD habit

--- client/reducers/userReducer.js
+++ client/reducers/userReducer.js
@@ -1,4 +1,4 @@
-const userReducer = (state = [], action) => {
+const userReducer = (state = { habits: [] }, action) => {
   switch (action.type) {
     case 'SET_USER':
       return action.user
--- client/reducers/userReducer.test.js
+++ client/reducers/userReducer.test.js
@@ -1,8 +1,38 @@
 import userReducer from './userReducer'
+import { addHabit, updateTheHabit, deleteTheHabit } from '../actions'
 
 describe('userReducer', () => {
-  test('initial state is an empty array', () => {
-    const state = userReducer(undefined, { type: '_INIT_' })
-    expect(state).toEqual([])
+  test('initial state is an object with a habits property array', () => {
+    const state = userReducer({ habits: [] }, { type: '_INIT_' })
+    expect(state).toEqual({ habits: [] })
+  })
+
+  test('ADD_HABIT adds a habit to the state', () => {
+    const fakeHabit = { title: 'take dog for walk everyday' }
+    const action = addHabit(fakeHabit)
+    const state = userReducer({ habits: [] }, action)
+    expect(state.habits).toHaveLength(1)
+  })
+
+  test('UPDATE_HABIT overwrites habit in the state', () => {
+    const initialHabits = [
+      { id: 1, title: 'Gym everyday!!' },
+      { id: 2, title: 'Drink 8 glasses of water per-day' }
+    ]
+    const newHabit = { id: 1, title: 'Gym weekly lol' }
+    const action = updateTheHabit(newHabit)
+    const state = userReducer({ habits: initialHabits }, action)
+    expect(state.habits[0]).toEqual(newHabit)
+  })
+
+  test('DELETE_HABIT removes habit with matching id from state', () => {
+    const initialHabits = [
+      { habitId: 1, title: 'go for daily walks' },
+      { habitId: 2, title: 'Eat a vegan meal daily' },
+      { habitId: 3, title: 'drink 10 beers daily' }
+    ]
+    const action = deleteTheHabit(3)
+    const state = userReducer({ habits: initialHabits }, action)
+    expect(state.habits).toHaveLength(2)
   })
 })
