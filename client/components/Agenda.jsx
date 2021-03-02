bec2e569c41e317cb8069409b07d32ab45f3d65a
mijburgh-beukes <mijburgh.beukes@gmail.com>
2021-03-02 21:41:07 +1300

    Fix broken dev changes

--- client/components/Agenda.jsx
+++ client/components/Agenda.jsx
@@ -10,10 +10,9 @@ function Agenda ({ user }) {
   const lengthOfOrderedHabits = orderedHabits ? orderedHabits.length - 1 : null
 
   // TODO: Possible opportunity for factoriseation
-  const habitWithHighestGC = orderedHabits ? orderedHabits[lengthOfOrderedHabits] : null
+  const habitWithHighestGC = orderedHabits ? lengthOfOrderedHabits : null
   const habitWithLowestGC = orderedHabits ? orderedHabits[0] : null
   const habitWith2ndLowestGC = orderedHabits ? orderedHabits[1] : null
-
   return (
     <div className="bg-white shadow-sm rounded-3 px-3 pb-1 pt-2 mb-3 text-midnight">
       <div className="row">
