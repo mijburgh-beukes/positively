78c37e3ec5679447a1238fa75448b4f7c9cfa745
Warahi Spenceley <wizzzo@protonmail.com>
2021-03-03 21:29:57 +1300

    merge w dev

--- client/components/HabitListItem.jsx
+++ client/components/HabitListItem.jsx
@@ -24,28 +24,28 @@ function Habit ({ dispatch, habit, user }) {
   }
 
   return (
-      <Tippy animation='perspective' theme='light' content={habit.description}>
-    <div className='accentBG text-white ps-2 pe-1 rounded-2 mb-2'>
-      <div className="ps-1 py-1 d-flex justify-content-between align-items-center">
-        <div className='counterContainer'>
-          <p>{`${topHabitTitle} - `}</p>
-          <div className='countIndicator'>
-            <p>{`${topHabitGoalCount} `}</p>
+    <Tippy animation='perspective' theme='light' content={habit.description}>
+      <div className='accentBG text-white ps-2 pe-1 rounded-2 mb-2'>
+        <div className="ps-1 py-1 d-flex justify-content-between align-items-center">
+          <div className='counterContainer'>
+            <p>{`${topHabitTitle} - `}</p>
+            <div className='countIndicator'>
+              <p>{`${topHabitGoalCount} `}</p>
+            </div>
           </div>
-        </div>
-        <div className="d-flex flex-row align-items-center">
-          {habit.goalCount > 0 &&
+          <div className="d-flex flex-row align-items-center">
+            {habit.goalCount > 0 &&
         <button className='btn resetButton me-2' onClick={handleCounterReset}>
           Reset Count
         </button>
-          }
-          <button className='plusButton me-1' onClick={handleTheCount}>
-            <span>+</span>
-          </button>
+            }
+            <button className='plusButton me-1' onClick={handleTheCount}>
+              <span>+</span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-          </Tippy>
+    </Tippy>
   )
 }
 
